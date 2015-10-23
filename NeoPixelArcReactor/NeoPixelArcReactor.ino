a3a028513aebd847c99811718a8813d0cffbe291
Michael Whitley <umlungu@gmail.com>
2015-10-23 11:29:52 -0500

    Update NeoPixelArcReactor.ino
    Added David's code for optional Spin Phase. Also introduced variables for ring start position.

--- NeoPixelArcReactor/NeoPixelArcReactor.ino
+++ NeoPixelArcReactor/NeoPixelArcReactor.ino
@@ -18,6 +18,9 @@ const float MODE_DURATION[NUM_MODES] = { 300, 0.3, 120, 0.3 };
 int mode = 0;
 unsigned long lastModeSwitch = millis();
 float hue = 0.33;
+int ringStart7 = 40; // Position of the first LED for the 7 LED Ring
+int ringStart16 = 24; // Position of the first LED for the 16 LED Ring
+int ringStart24 = 0; // Position of the first LED for the 24 LED Ring
 
 const uint8_t PROGMEM gamma[] = {
     0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
@@ -54,25 +57,25 @@ void loop() {
   {
     case 0:
       hue = 0.7;
-      oscillate(0, 1, 40, 255, 0.2, 0.0);
-      oscillate(1, 6, 10, 128, 0.2, 0.0);
-      spin( 7, 16, 3, 1.0, 64, 255, 1);
-      spin(23, 24, 3, 1.0, 64, 255, 0);
+      oscillate(ringStart7, 1, 40, 255, 0.2, 0.0);
+      oscillate(ringStart7+1, 6, 10, 128, 0.2, 0.0);
+      spin(ringStart16, 16, 3, 1.0, 64, 255, 1, 0.0);
+      spin(ringStart24, 24, 3, 1.0, 64, 255, 0, 2.0);
       break;
     case 2:
       hue = 0.7;
-      oscillate(0, 1, 0, 255, .3, TAU/16);
-      oscillate(1, 6, 0, 255, .3, -TAU/16);
-      oscillate(7, 16, 0, 255, .3, -TAU/8);
-      oscillate(23, 24, 0, 255, .3, -2 * TAU/8);
+      oscillate(ringStart7, 1, 0, 255, .3, TAU/16);
+      oscillate(ringStart7+1, 6, 0, 255, .3, -TAU/16);
+      oscillate(ringStart16, 16, 0, 255, .3, -TAU/8);
+      oscillate(ringStart24, 24, 0, 255, .3, -2 * TAU/8);
       break;
     case 1:
     case 3:
       hue = 0.3;
-      flicker(0, 1, 32, 255, 40);
-      flicker(7, 16, 32, 255, 40);
-      setPixelRange(1, 6, strip.Color(0,0,0));
-      setPixelRange(23, 24, strip.Color(0,0,0));
+      flicker(ringStart7, 1, 32, 255, 40);
+      flicker(ringStart16, 16, 32, 255, 40);
+      setPixelRange(ringStart7+1, 6, strip.Color(0,0,0));
+      setPixelRange(ringStart24, 24, strip.Color(0,0,0));
       break;
   }
   strip.show();
@@ -86,14 +89,14 @@ void oscillate(int startPixel, int pixelLength, int minAlpha, int maxAlpha, floa
   setPixelRange(startPixel, pixelLength, getColor(hue, alpha));
 }
 
-void spin(int startPixel, int pixelLength, int numPeaks, float freq, int minAlpha, int maxAlpha, int dir) {
+void spin(int startPixel, int pixelLength, int numPeaks, float freq, int minAlpha, int maxAlpha, int dir, float phase) {
   unsigned long time = millis();
   int alpha;
   int pos;
   
   for(int i=0; i<pixelLength; i++)
   {
-    alpha = (maxAlpha-minAlpha)*0.5*(1.0+sin(TAU*(numPeaks*(1.0*i/pixelLength)+(time/1000.0)*freq)))+minAlpha;
+    alpha = (maxAlpha-minAlpha)*0.5*(1.0+sin(TAU*(numPeaks*(1.0*i/pixelLength)+(time/1000.0)*freq)+phase))+minAlpha;
     if(dir == 0)
     {
       pos = startPixel+i;
@@ -151,5 +154,3 @@ uint32_t getColor(float hue, int alpha) {
     return Adafruit_NeoPixel::Color(0, w*3*brightness, (255-w*3)*brightness);
   }  
 }
-
-  
